2f2d2181c58ae3c367b7670c70c5976b8933e13b
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2023-09-21 17:49:31 +0200

    Bump version: 0.3.0 → 0.4.0

--- src/abba_python/__init__.py
+++ src/abba_python/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Nicolas Chiaruttini"""
 __email__ = 'nicolas.chiaruttini@epfl.ch'
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 
 from . import abba
 # from . import abba_python.abba_private, scijava_python_command, deepslice
